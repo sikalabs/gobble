829e6055b11a3dc5c96d11e12e1deec8dc9da3ca
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-03 17:14:31 +0100

    feat(exec_utils): Add RawExec, RawExecStdout methods

--- pkg/utils/exec_utils/exec_utils.go
+++ pkg/utils/exec_utils/exec_utils.go
@@ -2,20 +2,32 @@ package exec_utils
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/sikalabs/gobble/pkg/libtask"
 )
 
+func RawExec(cmd string, args ...string) error {
+	c := exec.Command(cmd, args...)
+	err := c.Run()
+	return err
+}
+
+func RawExecStdout(cmd string, args ...string) error {
+	c := exec.Command(cmd, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err := c.Run()
+	return err
+}
+
 func Exec(taskInput libtask.TaskInput, cmd string, args ...string) error {
 	if taskInput.Dry {
 		fmt.Println(strings.Join(append([]string{cmd}, args...), " "))
 		return nil
 	}
-	c := exec.Command(cmd, args...)
-	// c.Stdout = os.Stdout
-	// c.Stderr = os.Stderr
-	err := c.Run()
+	err := RawExec(cmd, args...)
 	return err
 }
