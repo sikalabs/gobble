7a2771caa71ef512a9d57861bf14fa9cc7379f2b
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-03 20:17:11 +0100

    feat(task/lib/apt_install): Add param state (present, absent)

--- pkg/task/lib/apt_install/apt_install.go
+++ pkg/task/lib/apt_install/apt_install.go
@@ -1,25 +1,47 @@
 package apt_install
 
 import (
+	"fmt"
+
 	"github.com/sikalabs/gobble/pkg/libtask"
 	"github.com/sikalabs/gobble/pkg/utils/exec_utils"
 )
 
 type TaskAptInstall struct {
-	Name string `yaml:"name"`
+	Name  string `yaml:"name"`
+	State string `yaml:"state"`
 }
 
 func Run(
 	taskInput libtask.TaskInput,
 	taskParams TaskAptInstall,
 ) libtask.TaskOutput {
-	err := exec_utils.Exec(
-		taskInput,
-		"ssh", taskInput.SSHTarget,
-		"apt-get", "install", "-y", "--no-install-recommends",
-		taskParams.Name,
-	)
+	var err error
+	if taskParams.State == "" {
+		taskParams.State = "present"
+	}
+	if taskParams.State == "present" {
+		err = exec_utils.Exec(
+			taskInput,
+			"ssh", taskInput.SSHTarget,
+			"apt-get", "install", "-y", "--no-install-recommends",
+			taskParams.Name,
+		)
+		return libtask.TaskOutput{
+			Error: err,
+		}
+	} else if taskParams.State == "absent" {
+		err = exec_utils.Exec(
+			taskInput,
+			"ssh", taskInput.SSHTarget,
+			"apt-get", "purge", "-y",
+			taskParams.Name,
+		)
+		return libtask.TaskOutput{
+			Error: err,
+		}
+	}
 	return libtask.TaskOutput{
-		Error: err,
+		Error: fmt.Errorf("unknown state: %s", taskParams.State),
 	}
 }
