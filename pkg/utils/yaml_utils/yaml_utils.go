10286b5f10cafc3e3a74b1329f978be6c9ae8032
Ondrej Sika <ondrej@ondrejsika.com>
2023-03-28 22:33:41 +0200

    feat(pkg/utils/yaml_utils): Add method MergeYAMLs

---
+++ pkg/utils/yaml_utils/yaml_utils.go
@@ -0,0 +1,35 @@
+package yaml_utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+func MergeYAMLs(paths []string) {
+	var merged map[interface{}]interface{}
+
+	data := []byte{}
+
+	for _, path := range paths {
+		file, err := ioutil.ReadFile(string(path))
+		if err != nil {
+			log.Fatalf("Failed to read %s: %v", string(path), err)
+		}
+		data = append(data, file...)
+	}
+
+	err := yaml.Unmarshal(data, &merged)
+	if err != nil {
+		log.Fatalf("Failed to merge YAML files: %v", err)
+	}
+
+	// Print the merged YAML
+	out, err := yaml.Marshal(merged)
+	if err != nil {
+		log.Fatalf("Failed to marshal merged YAML: %v", err)
+	}
+	fmt.Println(string(out))
+}
