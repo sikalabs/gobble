3dc5453747c66d4c620f250cddbc1a22456789e9
Ondrej Sika <ondrej@ondrejsika.com>
2023-03-30 21:23:59 +0200

    fix(pkg/run): Fix panic: assignment to entry in nil map

--- pkg/run/run.go
+++ pkg/run/run.go
@@ -88,6 +88,9 @@ func Run(
 }
 
 func mergeMaps(m1, m2 map[string]interface{}) map[string]interface{} {
+	if m1 == nil {
+		m1 = make(map[string]interface{})
+	}
 	deepCopyM1 := deepcopy.Copy(m1).(map[string]interface{})
 	for k, v := range m2 {
 		deepCopyM1[k] = v
