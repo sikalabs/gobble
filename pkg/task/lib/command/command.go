838e966e903d001d48200f527cfe4de6b1d6faf3
Ondrej Sika <ondrej@ondrejsika.com>
2023-04-09 09:38:36 +0200

    feat(pkg/task/lib/command): Add template support for task/command

--- pkg/task/lib/command/command.go
+++ pkg/task/lib/command/command.go
@@ -1,6 +1,9 @@
 package command
 
 import (
+	"bytes"
+	"text/template"
+
 	"github.com/sikalabs/gobble/pkg/libtask"
 	"github.com/sikalabs/gobble/pkg/utils/exec_utils"
 )
@@ -13,9 +16,26 @@ func Run(
 	taskInput libtask.TaskInput,
 	taskParams TaskCommand,
 ) libtask.TaskOutput {
-	err := exec_utils.SSH(
+
+	tmpl, err := template.New("cmd").Parse(taskParams.Cmd)
+	if err != nil {
+		return libtask.TaskOutput{
+			Error: err,
+		}
+	}
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, map[string]interface{}{
+		"Config": taskInput.Config,
+		"Vars":   taskInput.Vars,
+	})
+	if err != nil {
+		return libtask.TaskOutput{
+			Error: err,
+		}
+	}
+	err = exec_utils.SSH(
 		taskInput,
-		taskParams.Cmd,
+		buf.String(),
 	)
 	return libtask.TaskOutput{
 		Error: err,
