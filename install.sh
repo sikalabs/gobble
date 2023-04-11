4f06f66893d67a77fac7affbff4b828a655ce842
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-04-12 00:32:07 +0200

    cleaned unnecessary characters

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-VERSION=v0.1.0 && \
-OS=linux && \
-ARCH=amd64 && \
-BIN=install-slu && \
-curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
-tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
-rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
-sudo mv ${BIN} /usr/local/bin/ && \
-sudo /usr/local/bin/install-slu install && \
+VERSION=v0.1.0
+OS=linux
+ARCH=amd64
+BIN=install-slu
+curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz
+tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN}
+rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz
+sudo mv ${BIN} /usr/local/bin/
+sudo /usr/local/bin/install-slu install
 sudo /usr/local/bin/slu install-bin gobble
